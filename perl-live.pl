520f75e97d9ec16444be1fb58c3d3b3e85621839
vividsnow <vividsnow@gmail.com>
2015-04-15 01:23:04 +0300

    rework: persistent hints and utf-8 support

--- perl-live.pl
+++ perl-live.pl
@@ -1,24 +1,29 @@
-use v5.12;
-use warnings;
-use AnyEvent;
-use PadWalker 'peek_my';
-use Package::Stash;
+use strict; use warnings;
+
+package z;
 use Getopt::Std 'getopts';
+our ($st, $in, $hint, $buf, $ctx, $w) = (Package::Stash->new('main'), {});
+getopts( g => $in );
 
-getopts( g => my $Zinput = {} );
-my $Zstash = Package::Stash->new('main');
+package main;
+use utf8; use Encode 'decode_utf8';
+# uncomment if need by default
+# use feature qw'say state';
+# use experimental qw'smartmatch autoderef switch postderef lexical_subs';
+use PadWalker 'peek_my'; use Package::Stash ();
+use AnyEvent;
 
-my $Zwatcher = AE::io *STDIN, 0, sub {
-    state ($Zbuf, $Zcontext);
+$z::w = AE::io *STDIN, 0, sub {
     if (eof()) {
         scalar <>;
-        eval substr($Zbuf, 0, length $Zbuf, '').'; $Zcontext = peek_my(0)';
-        map { 
-            $Zstash->add_symbol($_, $Zcontext->{$_}) 
-        } grep { 
-            !($_ ~~ [qw($Zbuf $Zcontext $Zinput $Zstash)])
-        } keys$Zcontext if $Zinput->{g};
-        warn if $@;
-    } else { $Zbuf .= scalar <> }
-};
+        BEGIN { ($^H,${^WARNING_BITS},%^H) = @{$z::hint} if $z::hint }
+        eval 'BEGIN { ($^H,${^WARNING_BITS},%^H) = @{$z::hint} if $z::hint }'
+             .decode_utf8(substr($z::buf, 0, length $z::buf, ''))
+             .'; $z::ctx = peek_my(0);'
+             .'BEGIN { $z::hint = [$^H, ${^WARNING_BITS}, %^H] }';
+        map $z::st->add_symbol($_, $z::ctx->{$_}),
+        keys%$z::ctx if $z::in->{g};
+        warn if $@ }
+    else { $z::buf .= scalar <> } };
+
 AE::cv->recv;
