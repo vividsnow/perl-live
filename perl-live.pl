821358de6e9a013d1167d0edf46525212c79f288
vividsnow <vividsnow@gmail.com>
2024-04-11 00:56:18 +0300

    support v5.38

--- perl-live.pl
+++ perl-live.pl
@@ -1,7 +1,7 @@
 use utf8; use strict; use warnings;
 # uncomment if need by default
 # use feature qw'say state';
-# use experimental qw'smartmatch autoderef switch postderef lexical_subs';
+# use experimental qw'smartmatch switch lexical_subs declared_refs defer';
 use Encode 'decode_utf8';
 use Getopt::Std 'getopts';
 use PadWalker 'peek_my'; use Package::Stash ();
@@ -13,7 +13,7 @@ getopts( g => $z::in );
 
 $z::w = AE::io *STDIN, 0, sub {
     if (eof()) {
-        scalar <>;
+        STDIN->clearerr;
         eval 'BEGIN { ($^H,${^WARNING_BITS},%^H) = @{$z::hint} if $z::hint }'
              .decode_utf8(substr($z::buf//='', 0, length $z::buf, ''))
              .'; package main; $z::ctx = peek_my(0);'
