dd20fda1858e69c5abb637ffcd02d293f2d632a0
Pierre Rouleau <prouleau001@gmail.com>
2025-02-04 09:17:26 -0500

    Reformat.
    Although new format takes more vertical space it helps working with some
    tools.

--- perl-live.el
+++ perl-live.el
@@ -23,39 +23,64 @@
 (require 'comint)
 (require 'ansi-color)
 
-(defcustom perl-live-bin
-  (if (featurep 'perlenv) (perlenv-get-perl-path) "perl")
-  "path to perl binary"
+(defcustom perl-live-bin (if (featurep 'perlenv) (perlenv-get-perl-path) "perl")
+  "Path to perl binary, use perlenv by default."
   :group 'perl-live
-  :type 'string) ; use perlenv by default
-(defcustom perl-live-script
-  (format "%sperl-live.pl" (file-name-directory (or load-file-name buffer-file-name)))
+  :type 'string)
+
+(defcustom perl-live-script (format "%sperl-live.pl"
+                                    (file-name-directory
+                                     (or load-file-name buffer-file-name)))
   "path to script for live coding"
   :group 'perl-live
   :type 'string)
-(defcustom perl-live-switches "-g" "live script switches" ; -g for globalize context
+
+(defcustom perl-live-switches "-g"
+  "live script switches, -g for globalize context"
   :group 'perl-live
   :type 'string)
 
-(defconst perl-live-name "perl live" "name for process and buffer")
+(defconst perl-live-name "perl live"
+  "name for process and buffer")
 
-(defun perl-live-eval (text) "eval string in perl interpreter" (interactive "s")
+(defun perl-live-eval (text)
+  "Eval string in perl interpreter."
+  (interactive "s")
   (message (format "sending %d chars" (length text))) ; notify minibuffer
   (process-send-string perl-live-name (format "%s\n" text))
   (process-send-eof perl-live-name))
-(defun perl-live-eval-region (start end) "eval region" (interactive "r")
+
+(defun perl-live-eval-region (start end)
+  "Eval region."
+  (interactive "r")
   (perl-live-eval (buffer-substring-no-properties start end))
   (deactivate-mark))
-(defun perl-live-eval-line () "eval line" (interactive)
-  (save-excursion ((lambda (p) (perl-live-eval-region (car p) (cdr p))) (bounds-of-thing-at-point 'line)))
-  (forward-line))
-(defun perl-live-eval-region-or-line () "eval line or region" (interactive)
-  (if (and transient-mark-mode mark-active)
-      (perl-live-eval-region (region-beginning) (region-end)) (perl-live-eval-line)))
-(defun perl-live-eval-sexp () "eval between braces" (interactive)
-  (save-excursion ((lambda (p) (perl-live-eval-region (+ 1 (car p)) (- (cdr p) 1))) (bounds-of-thing-at-point 'sexp))))
 
-(defun perl-live-run () "run perl-live commint session" (interactive)
+(defun perl-live-eval-line ()
+  "Eval line."
+  (interactive)
+  (save-excursion
+    ((lambda (p) (perl-live-eval-region (car p) (cdr p)))
+     (bounds-of-thing-at-point 'line)))
+  (forward-line))
+
+(defun perl-live-eval-region-or-line ()
+  "Eval line or region."
+  (interactive)
+  (if (and transient-mark-mode mark-active)
+      (perl-live-eval-region (region-beginning) (region-end))
+    (perl-live-eval-line)))
+
+(defun perl-live-eval-sexp ()
+  "Eval between braces."
+  (interactive)
+  (save-excursion
+    ((lambda (p) (perl-live-eval-region (+ 1 (car p)) (- (cdr p) 1)))
+     (bounds-of-thing-at-point 'sexp))))
+
+(defun perl-live-run ()
+  "Run perl-live commint session."
+  (interactive)
   (if (string= (process-status perl-live-name) "run")
       (message "already run")
     (if (featurep 'comint)
@@ -63,10 +88,10 @@
           (make-comint perl-live-name perl-live-bin nil perl-live-script perl-live-switches)
           (with-current-buffer (format "*%s*" perl-live-name)
             (mapcar (lambda (v) (set (make-local-variable v) 't))
-             (list 'ansi-color-for-comint-mode
-               'comint-scroll-to-bottom-on-input
-               'comint-scroll-to-bottom-on-output
-               'comint-move-point-for-output))
+                    (list 'ansi-color-for-comint-mode
+                          'comint-scroll-to-bottom-on-input
+                          'comint-scroll-to-bottom-on-output
+                          'comint-move-point-for-output))
             (set (make-local-variable 'comint-input-sender)
                  '(lambda (proc string)
                     (comint-send-string proc (format "%s\n" string))
@@ -74,12 +99,18 @@
       (start-process perl-live-name (format "*%s*" perl-live-name) perl-live-bin perl-live-script perl-live-switches))
     (message (format "check output at *%s* buffer" perl-live-name))))
 
-(defun perl-live-stop () "stop perl-live session" (interactive)
+(defun perl-live-stop ()
+  "stop perl-live session"
+  (interactive)
   (delete-process perl-live-name)
   (message (format "process %s killed" perl-live-name)))
 
-(defun perl-live-restart () "restart perl-live session" (interactive)
-  (delete-process perl-live-name) (sleep-for 0 100) (perl-live-run))
+(defun perl-live-restart ()
+  "restart perl-live session"
+  (interactive)
+  (delete-process perl-live-name)
+  (sleep-for 0 100)
+  (perl-live-run))
 
 
 (if (featurep 'cperl-mode)
